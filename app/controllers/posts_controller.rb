1814d5820c3e352cfe6a0d25b35db2ffa12a1563
yuta-eng-prg <horii6773@gmail.com>
2020-07-27 14:30:25 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all  # すべてのレコードを@postsに代入
+  end
+
+  def new
+    
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+
+end
